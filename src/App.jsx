ea4ad2c71fc4617eb69af6ba69a04c0101c85820
Saurav Burjwal <sauravburjwal@gmail.com>
2023-11-28 22:04:45 +0530

    add-about-us

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './Components/Navbar/Navbar';
 import HeroSection from './Components/HeroSection/HeroSection';
+import AboutUs from './Components/AboutUs/AboutUs';
 import Footer from './Components/Footer/Footer';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <HeroSection />
+      <AboutUs />
       <Footer />
     </>
   );
---
+++ src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,24 @@
+import './AboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us" id="about-us">
+      <div className="about-us-heading">
+        <h2>
+          Let’s Introduce <br /> Ourself
+        </h2>
+      </div>
+      <div className="vertical-line"></div>
+      <div className="about-us-text">
+        <h3>Criminal Lawyer</h3>
+        <p>
+          Amet minim mollit non deserunt ullamco est <br /> sit aliqua dolor do
+          amet sint. Velit officia consequatduis <br /> enim velit mollit
+          Exercitation.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
